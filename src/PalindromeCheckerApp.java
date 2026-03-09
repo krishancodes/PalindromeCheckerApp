bf2163b93da4673a769715103778aaf9073a7bac
krishancodes <kkumar49122@gmail.com>
2026-03-09 14:37:23 +0530

    Base application setup

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,10 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Palindrome Checker App");
+        System.out.println("This program checks whether a word is palindrome or not");
+
+    }
+
+}
