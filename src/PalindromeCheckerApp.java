5487076e6c4c0e3f65a696190fd3b24d209f2bf9
krishancodes <kkumar49122@gmail.com>
2026-03-09 15:50:39 +0530

    UC2: Hardcoded palindrome validation

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,11 +1,39 @@
+/**
+ * MAIN CLASS - PalindromeCheckerApp
+ *
+ * Use Case 2: Hardcoded Palindrome Validation
+ *
+ * Description:
+ * Demonstrates palindrome validation using a hardcoded string.
+ */
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Palindrome Checker App");
-        System.out.println("This program checks whether a word is palindrome or not");
-        System.out.println("UC1: Welcome module loaded");
+        System.out.println("Palindrome Checker App");
+        System.out.println("-----------------------");
+
+        // Hardcoded string
+        String input = "madam";
+
+        boolean isPalindrome = true;
+
+        // Loop till half of the string
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - i - 1)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is NOT a Palindrome");
+        }
 
     }
-
 }
